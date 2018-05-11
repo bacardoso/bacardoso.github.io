2605fcafd7a7f8b3f5509bbfbc258a84115afd42
Ygor Silva <ygor.98@outlook.com>
2018-05-11 00:03:42 -0300

    Mudança laranja pra preto lavado

--- js/creative.js
+++ js/creative.js
@@ -58,8 +58,8 @@
 
   //Show/hide topic on clicking
   $('a.toogle-topic').click(function() {
-    var gallery = $(this).parents('.portfolio-topic').find('.portfolio-box');
-    if($(gallery).is(':hidden')) $(gallery).slideDown('slow', function(){$(this).css({'display': 'block'})});
+    var gallery = $(this).parents('.portfolio-topic').children('.popup-gallery');
+    if($(gallery).is(':hidden')) $(gallery).slideDown('slow', function(){$(this).css({'display': 'initial'})});
     else $(gallery).slideUp();
     console.log($(gallery));
   });
